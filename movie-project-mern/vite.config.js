7e0247f254b80f3e2cf23870f321e2e333b378d8
Sudharshna Lakshmi S <sudharshnalakshmi@gmail.com>
2024-10-17 14:45:06 +0530

    Updated

--- movie-project-mern/vite.config.js
+++ movie-project-mern/vite.config.js
@@ -5,6 +5,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 3000
+    port: 80
   },
+  build: {
+    outDir: "build"  // Output directory for the build
+  }
 })
